e694d8ef98bd4bc092740163b6d24ad86c83bb5c
Filip <Filip>
2024-07-22 19:26:22 +0100

    mainnet deployment

--- truffle-config.js
+++ truffle-config.js
@@ -17,6 +17,11 @@ module.exports = {
       port: 80,
       network_id: '97'
     },
+    mainnet: { //for verification only
+      host: "mainnet.infura.io/v3/cb13153cb96549289f1c14013f9b5c42",
+      port: 80,
+      network_id: '1'
+    },
     bsc: { //for verification only
       host: "bsc-mainnet.public.blastapi.io",
       port: 80,
@@ -29,10 +34,11 @@ module.exports = {
   },
 
   //npx truffle run verify SMToken@0x7db970fdaf01c9b80c391158c5007c46aeab98ad --network bsc
+  //npx truffle run verify SMToken@0x05499f573c76edb076a5461e2956f2b9db386930 --network mainnet
   plugins: ['truffle-plugin-verify'],
   api_keys: {
-    etherscan: '61ZSB6UPFZPQHFS58MUVITVKEYFDQFX3JM' //for BSC
-    //etherscan: '3K4VZT8MB1TYKVG7H4NC4T9P3RK2THTBCJ' //for ETH
+    //etherscan: '61ZSB6UPFZPQHFS58MUVITVKEYFDQFX3JM' //for BSC
+    etherscan: '3K4VZT8MB1TYKVG7H4NC4T9P3RK2THTBCJ' //for ETH
   },
   // Set default mocha options here, use special reporters etc.
   mocha: {
